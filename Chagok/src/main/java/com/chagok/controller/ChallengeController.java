e4662f1246ea0b0238405a5fd21b0ce335985326
waterfall220525@gmail.com <waterfall220525@gmail.com>
2022-12-27 12:08:27 +0900

    test

--- Chagok/src/main/java/com/chagok/controller/ChallengeController.java
+++ Chagok/src/main/java/com/chagok/controller/ChallengeController.java
@@ -1,15 +1,19 @@
 package com.chagok.controller;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.chagok.domain.ChallengeVO;
 import com.chagok.service.ChallengeService;
-import com.chagok.service.UserService;
 
 @Controller
 @RequestMapping("/challenge/*")
@@ -25,6 +29,39 @@ public class ChallengeController {
 	public String plusfeedGET() throws Exception{
 		
 		return "/challenge/plusFeed";
+	}
+//	// http://localhost:8080/challenge/plusdetail
+//	@GetMapping(value = "/plusdetail")
+//	public String plusdetailGET(Model model,@RequestParam("cno") int cno, HttpSession session) throws Exception{
+//		mylog.debug("plusdetailGET 호출");
+//		mylog.debug(cno+"");
+//		
+//		ChallengeVO vo = service.getChallengeInfo(cno);
+//		
+//		model.addAttribute("vo", vo); // plusdetail로 정보전달
+//		
+//		return "/challenge/plusdetail";
+//	}
+	
+	@PostMapping(value="/plusdetailPOST")
+	public String plusdetailPOST() throws Exception {
+		
+		return "/challenge/plusdetail";
+	}
+	
+	// http://localhost:8080/challenge/minusdetail
+	@GetMapping(value = "/minusdetail")
+	public String minusdetailGET() throws Exception{
+		mylog.debug("minusdetailGET 호출");
+		
+		return "/challenge/minusdetail";
+	}
+	
+	// http://localhost:8080/challenge/echo
+	@GetMapping(value = "/echo")
+	public String echoGET() throws Exception{
+		
+		return "/challenge/echo";
 	}
 }
 
