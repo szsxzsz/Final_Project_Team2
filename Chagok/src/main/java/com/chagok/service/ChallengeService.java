5878a2ebc0c6711487ce77a22da60e713b21cfbb
waterfall220525@gmail.com <waterfall220525@gmail.com>
2022-12-28 01:03:26 +0900

    221228_ahr 서버오류 수정

--- Chagok/src/main/java/com/chagok/service/ChallengeService.java
+++ Chagok/src/main/java/com/chagok/service/ChallengeService.java
@@ -8,14 +8,9 @@ import com.chagok.domain.PlusVO;
 public interface ChallengeService {
 	
 	// 챌린지 정보 불러오기 (사진 / 카테고리 / 제목 / 장 / 인원 / 기간 / 시작일 /예치금)
-<<<<<<< HEAD
 	public ChallengeVO getChallengeInfo(Integer cno);
 	
 	// [저축형]챌린지 참가자 조회 
 	public List<PlusVO> getPlusPeople(Integer cno);
-=======
-		public ChallengeVO getChallengeInfo(Integer cno);
-	
 
->>>>>>> branch 'develop_community' of https://github.com/Ju-Yeongmin/Final_Project_Team2.git
 }
